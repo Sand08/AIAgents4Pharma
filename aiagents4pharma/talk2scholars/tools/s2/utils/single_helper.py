1fe20b532fb93c8c9aeb9ebce9122447b11f1a7f
Sandeep Sharma <sandeepsharma2096@gmail.com>
2025-05-22 10:51:38 +0200

    H index

--- aiagents4pharma/talk2scholars/tools/s2/utils/single_helper.py
+++ aiagents4pharma/talk2scholars/tools/s2/utils/single_helper.py
@@ -134,14 +134,18 @@ class SinglePaperRecData:
                     f"{author.get('name', 'N/A')} (ID: {author.get('authorId', 'N/A')})"
                     for author in paper.get("authors", [])
                 ],
-
                 "Max H-Index": (
-                    lambda vals: max(vals) if vals else "N/A"
-                )([
-                    int(author["hIndex"])
+                max(
+                    [
+                        int(author["hIndex"])
+                        for author in paper.get("authors", [])
+                        if str(author.get("hIndex", "")).isdigit()
+                    ]
+                ) if any(
+                    str(author.get("hIndex", "")).isdigit()
                     for author in paper.get("authors", [])
-                    if str(author.get("hIndex", "")).isdigit()
-                ]),
+                ) else "N/A"
+            ),
                 "URL": paper.get("url", "N/A"),
                 "arxiv_id": paper.get("externalIds", {}).get("ArXiv", "N/A"),
             }
